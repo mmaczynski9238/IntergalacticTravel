3bb0bfc3f0305e09fab42dcc58f35fe30478b7d8
student3 <student3@jh-mbair-158-17.d214.org>
2015-11-06 14:30:07 -0600

    Created new view controller and connected two buttons

---
+++ InterGalacticTravel/imageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  imageViewController.swift
+//  InterGalacticTravel
+//
+//  Created by student3 on 11/6/15.
+//  Copyright © 2015 JohnHerseyHighSchool. All rights reserved.
+//
+
+import UIKit
+
+class imageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
